7056b65ff34d24d26f84b49dbe657f3d1fc6f330
Dev <francis.neequaye@gmail.com>
2024-01-02 01:25:07 +0000

    ModelRegistryService

--- src/svc/pred/model_registry.py
+++ src/svc/pred/model_registry.py
@@ -2,31 +2,33 @@ import torch
 from pathlib import Path
 from src.svc.model.simple_classifier import SimpleBinaryClassifier
 
-
 class ModelRegistryService:
     """
-    Service class for managing and providing access to different machine learning models.
+    Service class for managing and providing access to different machine learning models,
+    supporting both local and Hugging Face-hosted models.
     """
 
     def __init__(self):
         self.models = {}  # A dictionary to store model instances
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    def load_model(self, model_identifier, input_size):
+    def load_model(self, model_identifier, input_size, model_path=None):
         """
         Loads and returns the model instance based on the provided identifier.
 
         Args:
             model_identifier (str): A unique identifier for the model.
             input_size (int): The number of input features the model expects.
+            model_path (str, optional): The path to the model's state dictionary. If not provided,
+                                        the local path will be used.
 
         Returns:
             torch.nn.Module: The loaded and initialized model.
         """
         if model_identifier not in self.models:
-            # Load the model here, for example:
             model = SimpleBinaryClassifier(input_size=input_size)
-            state_dict = torch.load(self.get_model_path(model_identifier), map_location=self.device)
+            state_dict_path = model_path or self.get_model_path(model_identifier)
+            state_dict = torch.load(state_dict_path, map_location=self.device)
             model.load_state_dict(state_dict)
             model.to(self.device)
             self.models[model_identifier] = model
@@ -42,12 +44,15 @@ class ModelRegistryService:
         Returns:
             str: The file path of the model's state dictionary.
         """
-        root_dir_path = Path(__file__).parent
-        return f"{root_dir_path}models/{model_identifier}.pt"
-
+        root_dir_path = Path(__file__).resolve().parent
+        model_dir = root_dir_path / "models"
+        model_dir.mkdir(exist_ok=True)
+        return model_dir / f"{model_identifier}.pt"
 
 if __name__ == '__main__':
     model_registry_service = ModelRegistryService()
-    path = model_registry_service.get_model_path(model_identifier='IntraDayForexPredictor_v4.0.10')
-    print(path)
+    model_identifier = 'IntraDayForexPredictor_v4.0.10'
+    model_path = model_registry_service.get_model_path(model_identifier=model_identifier)
+    print(model_path)
+    model = model_registry_service.load_model(model_identifier=model_identifier, input_size=10)
 
